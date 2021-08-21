ab408525f88a1969d1cee07f425f4730528e3ac8
Seunghoon-Ahn <seong505@gmail.com>
2021-08-21 23:35:55 +0900

    src-upload

---
+++ src/test/java/com/chap2/test/web/HelloControllerTest.java
@@ -0,0 +1,8 @@
+package com.chap2.test.web;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class HelloControllerTest {
+}
