b7dc50e7decd98b7d2a9e3e90ba9c77e172d1ca2
Seunghoon-Ahn <seong505@gmail.com>
2021-09-16 22:06:20 +0900

    20210916

--- src/main/java/com/chap2/test/service/posts/PostsService.java
+++ src/main/java/com/chap2/test/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.chap2.test.service.posts;
 
+import com.chap2.test.domain.posts.Posts;
 import com.chap2.test.domain.posts.PostsRepository;
+import com.chap2.test.web.dto.PostsResponseDto;
 import com.chap2.test.web.dto.PostsSaveRequestDto;
+import com.chap2.test.web.dto.PostsUpdateRequestDto;
 import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,5 +19,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/chap2/test/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.chap2.test.web.dto;
+
+import com.chap2.test.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
