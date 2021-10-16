828d384821b22895c999200dced6a44ef41372b3
Seunghoon-Ahn <seong505@gmail.com>
2021-10-16 17:29:52 +0900

    20210930-username수정

--- src/main/java/com/chap2/test/service/posts/PostsService.java
+++ src/main/java/com/chap2/test/service/posts/PostsService.java
@@ -6,9 +6,9 @@ import com.chap2.test.web.dto.PostsListResponseDto;
 import com.chap2.test.web.dto.PostsResponseDto;
 import com.chap2.test.web.dto.PostsSaveRequestDto;
 import com.chap2.test.web.dto.PostsUpdateRequestDto;
-import javafx.geometry.Pos;
+
 import lombok.RequiredArgsConstructor;
-import net.bytebuddy.asm.Advice;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
--- src/main/java/com/chap2/test/web/IndexController.java
+++ src/main/java/com/chap2/test/web/IndexController.java
@@ -4,7 +4,7 @@ import com.chap2.test.config.auth.LoginUser;
 import com.chap2.test.config.auth.dto.SessionUser;
 import com.chap2.test.service.posts.PostsService;
 import com.chap2.test.web.dto.PostsResponseDto;
-import javafx.geometry.Pos;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
